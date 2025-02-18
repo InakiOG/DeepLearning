ef81a54fdda22352edf8dd5a5a2c20c824b804f0
OROZCO GARCIA <is719276@iteso.mx>
2025-02-17 21:53:27 -0600

    Pending alex net

--- AlexNet.py
+++ AlexNet.py
@@ -4,44 +4,43 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
-# Define the LeNet-5 model
-
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
-    super(AlexNet, self).__init__()
-    self.features = nn.Sequential(
-        # Conv 1
-        nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2),
-
-        # Conv 2
-        nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2),
-
-        # Conv 3
-        nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-        nn.ReLU(inplace=True),
-
-        # Conv 4
-        nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-        nn.ReLU(inplace=True),
-
-        # Conv 5
-        nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2),
-
-        nn.Linear(9216, 4096),
-        nn.ReLU(inplace=True)
-        nn.Dropout(0.5),
-        nn.Linear(4096, 4096),
-        nn.ReLU(inplace=True),
-        nn.Linear(4096, num_classes)
-    )
+        super(AlexNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(9216, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, num_classes))
 
     def forward(self, x):
         out = self.layer1(x)
@@ -56,4 +55,26 @@ class AlexNet(nn.Module):
         return out
 
 
+# Transformations for the data
+transform = transforms.Compose([
+transforms.ToTensor(),
+transforms.Normalize((0.1307,), (0.3081,))
+]
+
+# Load the dataset
+trainset=torchvision.datasets.STL10(root='./data', train=True,
+download=True, transform=transform)
+trainloader=torch.utils.data.DataLoader(trainset,
+batch_size=64, shuffle=True)
+testset=torchvision.datasets.MNIST(root='./data', train=False,
+download=True, transform=transform)
+testloader=torch.utils.data.DataLoader(testset,
+batch_size=1000, shuffle=False)
+# Initialize the network, criterion and optimizer
+net=AlexNet()
+criterion=nn.CrossEntropyLoss()
+optimizer=optim.SGD(net.parameters(), lr=0.01, momentum=0.9
 torchvision.datasets.STL10(root='./data', download=True)
+
+
+https: // www.digitalocean.com/community/tutorials/alexnet-pytorch
